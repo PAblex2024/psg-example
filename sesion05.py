34f144766a8e4b67dc6b5438b6e78928ec111379
Erick Mayta <erickpablomaytachurqui@gmail.com>
2024-05-26 19:36:07 -0400

    sesion05

---
+++ sesion05.py
@@ -0,0 +1,101 @@
+# Ejemplo de valor
+print(type(1))
+print(10)
+print(type(10))
+# variables
+variable=100
+print(variable)
+print(type(variable))
+# variable 20 es un entero
+variable_2=int(20)
+print(variable_2)
+print(type(variable_2))
+# valor 10 en base decimal
+print("base decimal")
+print(10)
+# valor 10 en binario
+print("base binaria")
+print(0b1010)
+# valor 10 en octal
+print("Base octal")
+print(0o12)
+# valor 10 en hexadecimal
+print("base hexadecimal")
+print(0xa)
+# entero con 60 digitos
+variable_3=12345678912345678912345678912345678912345678912345678912345556
+print(variable_3)
+print(type(variable_3))
+# valor 0.5 flotante
+print(0.5)
+print(type(0.5))
+# variable 0.100546 flotante
+variable_4=0.100546
+print(variable_4)
+print(type(variable_4))
+# variable 1 flotante
+variable_7=float(1)
+print(variable_7)
+print(type(variable_7))
+# precicion de 17 decimales
+variable_5=0.999999999999999
+print(variable_5)
+print(type(variable_5))
+# valor 2.0e-3
+variable_6=2.0e-3
+print(variable_6)
+print(type(variable_6))
+# operadores aritmeticos
+a=10
+b=3
+# suma
+print("SUMA")
+print(a+b)
+# resta
+print("resta")
+print(a-b)
+# multiplicacion
+print("multiplicacion")
+print(a*b)
+# division
+print("division")
+print(a/b)
+# potenciacion
+print("potencia")
+print(a**b)
+# modulo o residuo
+print("modulo o residuo")
+print(a%b)
+# division entera
+print("division entera")
+print(a//b)
+# PEMDSR   (PRIORIDADES ARITMETICAS)
+# UN CONTADOR TIENE 300 MIN Y SE LE SUMAN 3600 SEG CUANTAS HORAS EN TOTAL SON
+min=300
+seg=3600
+hor=(min/60)+((seg/60)/60)
+print(hor)
+# operadores de comparacion
+comparar=10
+print(comparar<10)
+print(comparar>10)
+print(comparar==10)
+print(comparar<=10)
+print(comparar>=10)
+print(comparar!=10)
+# comparar enteros con flotantes
+entero=10
+flotante=10.0
+print(entero<flotante)
+print(entero>flotante)
+print(entero==flotante)
+print(entero<=flotante)
+print(entero>=flotante)
+print(entero!=flotante)
+print(2.0 is 2)
+print(2.0==2)
+numero1=6
+numero2=2
+resultado=numero1/numero2
+print(resultado)
+print ((resultado%2)==2)
