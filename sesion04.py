742f765f6751931010e8734e9accb44adc4d9eb3
Erick Mayta <erickpablomaytachurqui@gmail.com>
2024-05-25 21:51:50 -0400

    sesion04

---
+++ sesion04.py
@@ -0,0 +1,9 @@
+# Example of execution flow
+#Instruction 1
+print("Start of the program")
+#Instruction 2
+print("Print second instruction")
+#Instruction 3
+print ("Hello, World")
+#Instruction 4
+print("end of the program")
